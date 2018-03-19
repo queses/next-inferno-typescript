dcb52af3c8c5525640460ba85fea9814605a30f4
Max Rattle <quesesr@gmail.com>
2018-03-19 21:42:37 +0500

    0.0.3 - Exports fixes

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 const withInferno = require('next-inferno')
 const withAwesomeTypescript = require("next-awesome-typescript");
 
-module.exports = withInferno(withAwesomeTypescript())
+module.exports = (next) => withInferno(withAwesomeTypescript(next))
